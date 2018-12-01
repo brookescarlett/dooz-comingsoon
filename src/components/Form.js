3405b952c037167abd16f658be17d53daeeea28a
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-12-01 14:57:47 -0500

    cleaned up console messages

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,14 +10,13 @@ export default class Form extends Component {
     }
 
     render() {
-        console.log(this.props.email)
         return (
 
                 <div id="mc_embed_signup">
-                    <form action="https://twitter.us19.list-manage.com/subscribe/post?u=df9cb29c90a8fb08314d143f8&amp;id=ace1c561d7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <form action="https://twitter.us19.list-manage.com/subscribe/post?u=df9cb29c90a8fb08314d143f8&amp;id=ace1c561d7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" noValidate>
                         <div id="mc_embed_signup_scroll">
                             <div class="mc-field-group">
-                                <input type="email" name="EMAIL" class="required email" value={this.props.email} id="mce-EMAIL" onChange={this.props.changeEmail} className="input-box" />
+                                <input type="email" name="EMAIL" value={this.props.email} id="mce-EMAIL" onChange={this.props.changeEmail} className="required email input-box" />
                             </div>
 
                             <div class="mc-field-group input-group">
@@ -25,11 +24,11 @@ export default class Form extends Component {
                             </div>
 
                             <div id="mce-responses" class="clear">
-                                <div class="response" id="mce-error-response" style={{display: "none"}}></div>
-                                <div class="response" id="mce-success-response" style={{display: "none"}}></div>
+                                <div className="response" id="mce-error-response" style={{display: "none"}}></div>
+                                <div className="response" id="mce-success-response" style={{display: "none"}}></div>
                             </div> 
-                            
-                            <div style={{position: "absolute", left: "-5000px", ariaHidden: "true"}}><input type="text" name="b_df9cb29c90a8fb08314d143f8_ace1c561d7" tabindex="-1" value=""/></div>
+
+                            <div style={{position: "absolute", left: "-5000px", ariaHidden: "true"}}><input type="text" name="b_df9cb29c90a8fb08314d143f8_ace1c561d7" tabIndex="-1" /></div>
                             <div class="clear"><input type="submit" value="sign up" name="subscribe" id="mc-embedded-subscribe" className="submit"/></div>
                         </div>
                     </form>
