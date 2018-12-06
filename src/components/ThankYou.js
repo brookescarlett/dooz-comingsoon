7cb153cbe02e7639eee2700594d4fcf24835c232
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-12-06 08:03:43 -0500

    locally store

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -5,13 +5,14 @@ import Footer from './Footer'
 
 
 class ThankYou extends Component {
+
     render() {
         return (
             <div className="background-img fixed top-0 left-0 right-0 bottom-0">
                 <div className="container flex flex-column items-around">
                     <SideBar 
                         currentPage={"ThankYou"}
-                        mainText={`Hi! ${localStorage.email} Thank you for signing up. Check your inbox for exclusive Dooz updates. See you in March 2019!`}
+                        mainText={`Hi! ${localStorage.email ? localStorage.email : null} Thank you for signing up. Check your inbox for exclusive Dooz updates. See you in March 2019!`}
                     />
                     <Footer />
                 </div>
