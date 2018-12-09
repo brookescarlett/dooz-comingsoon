cbb1b391eab27a6357196b9a0c082211bd4e6b76
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-12-09 15:40:12 -0500

    finally fixed social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react'
-import Facebook from '../assets/svgs/fb-icon.svg'
-import Instagram from '../assets/svgs/insta-ic.svg'
-import Twitter from '../assets/svgs/TwitterIcon.svg'
-
 
 
 export default class Footer extends Component {
@@ -13,9 +9,12 @@ export default class Footer extends Component {
             <div className="footer flex justify-between items-center">
                 { this.props.activeComponent === "Options" && window.innerWidth < 500 ? null : (
                     <div className="footer-icons flex">
-                        <img src={Facebook} alt="facebook-logo" className="social-icons" />
+                        {/* <img src={Facebook} alt="facebook-logo" className="social-icons" />
                         <img src={Instagram} alt="facebook-logo" className="social-icons" />
-                        <img src={Twitter} alt="facebook-logo" className="social-icons" />
+                        <img src={Twitter} alt="facebook-logo" className="social-icons" /> */}
+                        <div id="Facebook" className="social-icons"></div>
+                        <div id="Instagram" className="social-icons"></div>
+                        <div id="Twitter" className="social-icons"></div>
 
                     </div>
                 )}
