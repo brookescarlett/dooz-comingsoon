662ea63f9fe428cfe159f162610c4aa76b190ed3
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-12-11 10:11:16 -0500

    before i try and flex the side bar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,30 @@ export default class Footer extends Component {
             <div className="footer flex justify-between items-center">
                 { this.props.activeComponent === "Options" && window.innerWidth < 500 ? null : (
                     <div className="footer-icons flex">
-                        <img src={Facebook} alt="facebook-logo" className="social-icons" />
-                        <img src={Instagram} alt="facebook-logo" className="social-icons" />
-                        <img src={Twitter} alt="facebook-logo" className="social-icons" />
+                        <a 
+                            href="https://www.facebook.com/dooznyc"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                            <img src={Facebook} alt="facebook-logo" className="social-icons" />    
+                        </a>
+
+                        <a 
+                            href="https://www.instagram.com/dooznyc/?hl=en"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                            <img src={Instagram} alt="facebook-logo" className="social-icons" />    
+                        </a>
+
+
+                        <a 
+                            href="https://twitter.com/dooznyc"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                            <img src={Twitter} alt="facebook-logo" className="social-icons" />
+                        </a>
 
                     </div>
                 )}
